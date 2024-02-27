df848f3d614c1d14b932bbae4b8dd3f5c6f8376c
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-02-27 14:15:13 +0100

    update configuration loader and deleted old loader

--- map_reader.py
+++ map_reader.py
@@ -1,80 +1,88 @@
-def map_reader(map_dir: str) -> tuple:
+import map_gen
+
+def setup_scenario(map: str) -> tuple:
     """
     File reader which loads the specified map and coverts into useable structure for the program
 
     Generic file reader based on original reader within run_experiments.py
 
     Args:
-        param1: (string) directory pointing to the map 
+        param1: (string) Containts the full map content, where agent start and end locations are specified:
+        example:
+            4 7             # grid with 4 rows and 7 columns
+            @ @ @ @ @ @ @   # example row with obstacle in every column
+            @ . . . . . @   # example row with 5 free cells in the middle
+            @ @ @ . @ @ @
+            @ @ @ @ @ @ @
+            2               # 2 agents in this experiment
+            1 1 1 5         # agent 1 starts at (1,1) and has (1,5) as goal
+            1 2 1 4         # agent 2 starts at (1,2) and has (1,4) as goal
 
     Returns:
         tuple:
-            param1: (array) map loaded where walls (@) are converted to False and wakable locations (.) are converted to True
-            param2: (tuple) dimension of the map (rows, cols)
+            param1: (array) map loaded where walls (@) are converted to True and wakable 
+            locations (.) are converted to False. The array has the size equal to the first 
+            input line in the specified configuration file of the map.
+
+            param2: (array) Start positions of the various agents in the map (x, y)
+
+            param3: (array) Goal location for the various agents in the map (x, y)
 
     Raises:
-        FileNotFound: Exception if the map loaded is not found in the current directory
-        BaseException: Bad map detected, inconsistent size
+        BaseException: Bad map detected, inconsistent size , or impossible placement of agents
     """
-    my_map = []
+    my_map: list[list[bool]] = []
+    starts: list[int] = []
+    goals: list[int] = []
 
-    try:
-        with open(map_dir, "r") as file:
-            my_map_str = file.read()    # Local copy for error msg
-            file.seek(0)
-            for line in file.readlines():
-                line = line.rstrip(" \n").split(" ")
-                line = [True if el == "@" else False for el in line]
-                my_map.append(line)
+    file = map.splitlines()
 
-    except FileNotFoundError:
-        raise BaseException(f"{map_dir} doesn't exist")
+    line = 0
+    
+    dimension_map = [int(el) for el in file[line].split(" ")]
+    my_map_str = "\n".join(file[1:dimension_map[0]+1])
+
+    line += 1
+
+    for row in range(dimension_map[0]):
+
+        row_map = file[line + row].split(" ")
+        row_map = [True if el == "@" else False for el in row_map]
+        my_map.append(row_map)
 
     # Sanity check \\ mainly ensuring the size is okay
-    dimensions = (len(my_map), len(my_map[0]))
 
-    for id, line in enumerate(my_map, start=1):
+    for id, content in enumerate(my_map, start=1):
 
-        if len(line) != dimensions[1]:
+        if len(content) != dimension_map[1]:
             print(my_map_str)
-            raise BaseException(f"{map_dir}, has unequaly dimensions between lines detected on line: {id}")
-
-    return my_map, dimensions
-
-def load_agents_data(my_map, agents) -> tuple:
-    """
-    Load the agents and pass as useable structure for the code, written based on original formulation within run_experiment.py
-
-    Args:
-        param1: 
-
-    Returns:
-        param1:
-
-    Raises:
-        
-    """
-    starts: list  = []
-    goals: list   = []
+            raise BaseException(f"The map has unequaly dimensions between lines. Detected on line: {id}")
     
-    for id, agent in enumerate(agents, start=1):
-        x_pos = int(agent[0])
-        y_pos = int(agent[1])
+    line += dimension_map[0]
 
-        x_gl = int(agent[2])
-        y_gl = int(agent[3])
+    agent_amount = int(file[line])
+
+    line += 1
+
+    for id, agent in enumerate(range(agent_amount), start=1):
+        
+        sx, sy, gx, gy = [int(x) for x in file[line + agent].split(' ')]
+        starts.append((sx, sy))
+        goals.append((gx, gy))
 
         # Agent init located on invalid location
-        if my_map[y_pos][x_pos] == False:
+        if my_map[sy][sx] == True:
             raise BaseException(f"Agent_init {id} is located on invalid location")
         # Agent goal located on invalid location
-        if my_map[y_gl][x_gl] == False:
+        if my_map[gy][gx] == True:
             raise BaseException(f"Agent_goal {id} is located on invalid location")
-        
-        starts.append((x_pos, y_pos))
-        goals.append((x_gl,y_gl))
+    
+    return my_map, starts, goals
 
-    return starts, goals
 
 if __name__ == "__main__":
-    map_reader("testmap.map")
+    map = "maps/assignment_1.map"
+    test_map_1 = map_gen.MapGenerator(map)
+    file = test_map_1.generate()
+
+    config = setup_scenario(file)
