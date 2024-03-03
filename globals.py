2f4e3d3491a23863fb70063a51ce22b5c3a4aa09
LordNocturnus <felix@lagarden.de>
2024-03-03 14:49:40 +0100

    moved Prime numbers to globals.py file

---
+++ globals.py
@@ -0,0 +1,6 @@
+"""
+Two prime numbers are used to encode both the x and y coordinate into a single integer
+"""
+PRIMEX = 461
+
+PRIMEY = 463
--- map_gen.py
+++ map_gen.py
@@ -1,12 +1,5 @@
-from copy import deepcopy
 import numpy as np
-
-"""
-Two prime numbers are used to encode both the x and y coordinate into a single integer
-"""
-PRIMEX = 461
-
-PRIMEY = 463
+from globals import PRIMEX, PRIMEY
 
 
 class MapGenerator(object):
