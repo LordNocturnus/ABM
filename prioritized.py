8b664f384fe84e3c530a8c26a6cebdd5ee4c6461
LordNocturnus <felix@lagarden.de>
2024-03-28 15:59:07 +0100

    added recursive flag to prioritized

--- prioritized.py
+++ prioritized.py
@@ -34,11 +34,11 @@ class PrioritizedPlanningSolver(object):
         for goal in self.goals:
             self.heuristics.append(compute_heuristics(my_map, goal))
 
-    def find_solution(self) -> list[list[tuple[int, int]]]:
+    def find_solution(self, recursive: bool = True) -> list[list[tuple[int, int]]]:
         """ Finds paths for all agents from their start locations to their goal locations."""
 
         start_time = timer.time()
-        result = find_restricted_path(self.my_map, self.starts, self.goals, self.heuristics)
+        result = find_restricted_path(self.my_map, self.starts, self.goals, self.heuristics, recursive=recursive)
 
 
             ##############################
@@ -53,10 +53,10 @@ class PrioritizedPlanningSolver(object):
 
         self.CPU_time = timer.time() - start_time
 
-        print("\n Found a solution! \n")
-        print("CPU time (s):    {:.2f}".format(self.CPU_time))
-        print("Sum of costs:    {}".format(get_sum_of_cost(result)))
-        print(result)
+        #print("\n Found a solution! \n")
+        #print("CPU time (s):    {:.2f}".format(self.CPU_time))
+        #print("Sum of costs:    {}".format(get_sum_of_cost(result)))
+        #print(result)
         return result
 
 
@@ -64,7 +64,8 @@ def find_restricted_path(my_map: list[list[bool]],
                          starts: list[tuple[int, int]],
                          goals: list[tuple[int, int]],
                          heuristics: list[dict[tuple[int, int], int]],
-                         depth: int = 1) -> list[list[tuple[int, int]]]:
+                         depth: int = 0,
+                         recursive: bool = True) -> list[list[tuple[int, int]]]:
     result = []
     constraint_list: list[constraints.Constraint] = []
     if depth == math.factorial(len(starts)):
@@ -85,14 +86,15 @@ def find_restricted_path(my_map: list[list[bool]],
             # constraint_list.append({'positive': True, 'agent': a, 'loc': [path_vertex, path[min(i+1, len(path)-1)]], 'timestep': i + 1})
             constraint_list.append(Constraint(True, a, t + 1, path_vertex, path[min(t + 1, len(path) - 1)]))
 
-        for a2, p in enumerate(result[:-1]):
-            for t in range(max(len(path), len(p))):
-                if np.linalg.norm(np.asarray(get_location(p, t)) - np.asarray(get_location(path, t))) < 0.7:
-                    with warnings.catch_warnings():
-                        warnings.simplefilter("always")
-                        warnings.warn(f"COLLISION ON DEPTH {depth}!!!")
-                    starts[a], starts[a2] = starts[a2], starts[a]
-                    goals[a], goals[a2] = goals[a2], goals[a]
-                    heuristics[a], heuristics[a2] = heuristics[a2], heuristics[a]
-                    return find_restricted_path(my_map, starts, goals, heuristics, depth + 1)
+        if recursive:
+            for a2, p in enumerate(result[:-1]):
+                for t in range(max(len(path), len(p))):
+                    if np.linalg.norm(np.asarray(get_location(p, t)) - np.asarray(get_location(path, t))) < 0.7:
+                        with warnings.catch_warnings():
+                            warnings.simplefilter("always")
+                            warnings.warn(f"COLLISION ON DEPTH {depth}!!!")
+                        starts[a], starts[a2] = starts[a2], starts[a]
+                        goals[a], goals[a2] = goals[a2], goals[a]
+                        heuristics[a], heuristics[a2] = heuristics[a2], heuristics[a]
+                        return find_restricted_path(my_map, starts, goals, heuristics, depth + 1)
     return result
