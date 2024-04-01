8e92f1df98ddb4e1a16f27410fbdd514687310b1
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-04-01 14:56:50 +0200

    bases testing template

---
+++ tests/main.py
@@ -0,0 +1,22 @@
+import unittest
+import view
+import map_gen
+
+## Run test instruction
+# Set directory to parent directory: /ABM
+# Run test by running python -m tests.main
+
+class Test_View(unittest.TestCase):
+
+    def test_one(self):
+        a = 1
+        pass
+
+class Test_MapGenerator(unittest.TestCase):
+
+    def test_one(self):
+        a = 1
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
