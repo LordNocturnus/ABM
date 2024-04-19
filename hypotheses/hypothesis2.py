36691fc8227118dec05c69a21dadc7eb542648ad
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-04-19 15:49:04 +0200

    Hypo two data

--- hypotheses/hypothesis2.py
+++ hypotheses/hypothesis2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 import os
 import pathlib
@@ -65,8 +66,25 @@ class GlobalSolver:
         self.agents = self.data["agents"]
 
     def agents_vs_cost(self):
-        pass
+        """
+        Save data the box plots can be created where x are the number of agents, and y is the cost
+        """
+        # local = self.data.sort_values(by=["agents"])
+        unique = self.data["agents"].unique()
 
+        data = {}
+
+        for agents in range(1,21):
+            if agents in sorted(unique):
+                data[agents] = self.data.loc[self.data['agents'] == agents, "cost"]
+            else:
+                data[agents] = [0]
+
+        # fig, ax = plt.subplots()
+        # ax.boxplot(data.values())
+        # ax.set_xticklabels(data.keys())
+
+        return data
 
 class DistributedSolver(GlobalSolver):
 
@@ -80,6 +98,13 @@ class DistributedSolver(GlobalSolver):
         return ...
 
 
+def create_boxplot(data):
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Number of agents [-]')
+    ax.set_ylabel('Maximum path length [-]')
+    ax.boxplot(data.values())
+    ax.set_xticklabels(data.keys())
 
 
 # Global solvers
@@ -92,4 +117,54 @@ dist_prioritized = DistributedSolver("data/data_5min/results_dist_prioritized.cs
 dist_cbs_standard = DistributedSolver("data/data_5min/results_dist_cbs_standard.csv", "distributed cbs standard")
 dist_cbs_disjoint = DistributedSolver("data/data_5min/results_dist_cbs_disjoint.csv", "distributed cbs disjoint")
 
-print()
+## Agent vs cost
+prioritized_cost = prioritized.agents_vs_cost()
+cbs_standard_cost = cbs_standard.agents_vs_cost()
+cbs_disjoint_cost = cbs_disjoint.agents_vs_cost()
+
+dist_prioritized_cost = dist_prioritized.agents_vs_cost()
+dist_cbs_standard_cost = dist_cbs_standard.agents_vs_cost()
+dist_cbs_disjoint_cost = dist_cbs_disjoint.agents_vs_cost()
+
+# Create regular boxplots
+create_boxplot(prioritized_cost)
+create_boxplot(cbs_standard_cost)
+create_boxplot(cbs_disjoint_cost)
+create_boxplot(dist_prioritized_cost)
+create_boxplot(dist_cbs_standard_cost)
+create_boxplot(dist_cbs_disjoint_cost)
+
+# Compare outcome
+
+def compare_solvers(solver_1, solver_2, method=str):
+    comparison_cost = {"solver 1": 0,
+                       "equal": 0,
+                       "solver 2": 0}
+
+    for i in range(1, 21):
+        if method == "average":
+            delta = np.average(solver_1[i]) - np.average(solver_2[i])
+        elif method == "mean":
+            delta = np.mean(solver_1[i]) - np.mean(solver_2[i])
+        elif method == "Q1":
+            delta = np.quantile(solver_1[i], 0.25, axis=0) - np.quantile(solver_2[i], 0.25, axis=0)
+        elif method == "Q3":
+            delta = np.quantile(solver_1[i], 0.75, axis=0) - np.quantile(solver_2[i], 0.75, axis=0)
+        else:
+            raise "The methode does not exist"
+
+        if delta < 0:
+            comparison_cost["solver 1"] += 1
+        elif delta == 0:
+            comparison_cost["equal"] += 1
+        elif delta > 0:
+            comparison_cost["solver 2"] += 1
+
+    print(comparison_cost)
+
+compare_solvers(prioritized_cost, dist_prioritized_cost, "average")
+compare_solvers(prioritized_cost, dist_prioritized_cost, "mean")
+compare_solvers(prioritized_cost, dist_prioritized_cost, "Q1")
+compare_solvers(prioritized_cost, dist_prioritized_cost, "Q3")
+
+plt.show()
